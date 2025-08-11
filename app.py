0f6b7e409208144f39e2583c1683b1e4872c643f
manleja <homaleja.96@gmail.com>
2025-08-10 20:07:00 -0500

    Nueva funcion para cargar datos

--- app.py
+++ app.py
@@ -18,10 +18,17 @@ app.title = "Dashboard energia"
 server = app.server
 app.config.suppress_callback_exceptions = True
 
-
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
+    # Cargar el archivo CSV
+    df = pd.read_csv("datos_energia.csv")
+    # Convertir la columna 'time' a datetime
+    df["time"] = pd.to_datetime(df["time"])
+    # Establecer la columna 'time' como índice
+    df.set_index("time", inplace=True)
+    # Ordenar por fecha por si no está ordenado
+    df.sort_index(inplace=True)
+    return df
     
 
 # Cargar datos
@@ -240,4 +247,4 @@ def update_output_div(date, hour, proy):
 
 # Run the server
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
